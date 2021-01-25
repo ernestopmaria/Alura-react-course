ae8dc6d3ad9e9ec1eab3d8ba2b2d64fdc7553784
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-01-25 17:45:51 +0100

    up

--- quizhardcore/pages/index.js
+++ quizhardcore/pages/index.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 import db from '../db.json';
+import Widget from '../src/components/Widget/index';
+import GitHubCorner from '../src/components/GitHubCorner/index'
+import QuizBackground from '../src/components/QuizBackground/index'
+import Footer from '../src/components/Footer/'
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -21,40 +25,27 @@ background-position:center;
 
 `;
 
-const Widget =styled.div`
-margin-top:24px;
-margin-bottom:24px;
-border:1px solid #4caf50;
-background-color:#1c1814;
-border-radius:4px;
-overflow:hidden;
 
-h1, h2, h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`
-
- 
 
 export default function Home() {
   return (
     <BackgroundImage>
       <QuizContainer>
         <Widget>
-       <h1>Lendas da musica Brasileira</h1>
+        <Widget.Header>
+          <h1>Lendas da musica Brasileira</h1>
+        </Widget.Header>
+          <Widget.Content>
+               
        <p>testun jknlkjd kljkwjdkwjli</p>
+          </Widget.Content>
+      
         </Widget>
         <Widget>
-        <h1> BEST SONGS EVER</h1>
+        <Widget.Content>
+        
        <p>testun jknlkjd kljkwjdkwjli</p>
+       </Widget.Content>
         </Widget>
       </QuizContainer>
     </BackgroundImage>
